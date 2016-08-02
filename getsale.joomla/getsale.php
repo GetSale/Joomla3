6f2d97aea9a1d9199024da180168ec8c2ec1b985
gambit <chernozor@yandex.ru>
2016-08-02 18:45:09 +0300

    release

--- getsale.joomla/getsale.php
+++ getsale.joomla/getsale.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @version     1.0.0
- * @Project     getSale
- * @author      getsale.io
+ * @Project     GetSale
+ * @author      GetSale Team
  * @package
  * @copyright   Copyright (C) 2016 getsale.io. All rights reserved.
  * @license     GNU/GPL: http://www.gnu.org/copyleft/gpl.html
@@ -20,15 +20,175 @@ class plgSystemGetsale extends JPlugin
     public $projectId = '';
     public $error = '';
     public $regDomain = 'http://edge.getsale.io/';
-    public  $rtDomain = "//rt.edge.getsale.io/loader.js";
+    public $rtDomain = "//rt.edge.getsale.io/loader.js";
     public $int_scrpt = '';
+    public $VMAddToCartSelector = 'input.addtocart-button';
+    public $VMSuccessOrderSelector = 'button#checkoutFormSubmit';
+    public $VMViewCartSelector = 'body.view-cart';
+    public $VMDelFromCartSelector = '.vm2-remove_from_cart';
+    public $VMCatViewSelector = 'div.category-view';
+    public $jsCode2 = <<<EOD
+jQuery(document).ready(function(){
+    //hikashop
+        //////cat-view
+              if (jQuery('body.com_hikashop.view-category').length) {
+                           (function(w, c) {
+                    w[c] = w[c] || [];
+                    w[c].push(function(getSale) {
+                        getSale.event('cat-view');
+                    });
+                })(window, 'getSaleCallbacks');
+            }
+        //////item-view
+              if (jQuery('body.com_hikashop.view-product').length) {
+                            (function(w, c) {
+                w[c] = w[c] || [];
+                w[c].push(function(getSale) {
+
+                getSale.event('item-view')
+                    });
+                })(window, 'getSaleCallbacks');
+            }
+        //////add to cart
+            jQuery("input.button.hikashop_cart_input_button[name=add]").each(function() {
+                var my_funct = " getSale.event('add-to-cart');";
+            jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+            })
+        /////del from cart
+
+            jQuery(".hikashop_cart_product_quantity_delete").each(function() {
+                var my_funct = " getSale.event('del-from-cart');";
+            jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+            })
+         /////order_finish
+           if (jQuery('span#hikashop_purchaseorder_end_message').length) {
+
+            (function(w, c) {
+                w[c] = w[c] || [];
+                w[c].push(function(getSale) {
+
+                getSale.event('success-order')
+                    });
+                })(window, 'getSaleCallbacks');
+            }
+        ////user_reg
+            jQuery("input.button.hikashop_cart_input_button[name=register]").each(function() {
+                var my_funct = "getSale.event('user-reg');";
+                jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+            })
+
+    //joomshopping
+        //////view-cat
+        if (jQuery('div.jshop_list_category').length) {
+
+                        (function(w, c) {
+                    w[c] = w[c] || [];
+                    w[c].push(function(getSale) {
+                        getSale.event('cat-view');
+                    });
+                })(window, 'getSaleCallbacks');
+        }
+
+        //item-view
+        if (jQuery('div.jshop.productfull').length) {
+            (function(w, c) {
+                w[c] = w[c] || [];
+                w[c].push(function(getSale) {
+
+                getSale.event('item-view')
+                    });
+            })(window, 'getSaleCallbacks');
+        }
+        //add to cart
+        //add to cart at view item page
+        jQuery("a.btn.btn-success.button_buy").each(function() {
+            var my_funct = " getSale.event('add-to-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+
+        jQuery('.prod_buttons input.btn.btn-primary.button').each(function() {
+            var my_funct = " getSale.event('add-to-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+
+        jQuery("a[href*='/cart/delete']").each(function() {
+            var my_funct = " getSale.event('del-from-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+
+        jQuery("a[href*='/korzina/delete']").each(function() {
+            var my_funct = " getSale.event('del-from-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+
+        //user reg
+        if (jQuery("p.alert-message:contains('Учётная запись для вас была создана')").length) {
+            (function(w, c) {
+                w[c] = w[c] || [];
+                w[c].push(function(getSale) {
+                    getSale.event('user-reg');
+                });
+
+            })(window, 'getSaleCallbacks');
+        }
+        //order_finish
+
+        jQuery("input[name='finish_registration']").each(function() {
+            var my_funct = " getSale.event('success-order');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+
+    //virtuemart
+          jQuery('input.addtocart-button').click(function(){
+            getSale.event('add-to-cart');
+          });
+          jQuery('.vm2-remove_from_cart').click(function(){
+            getSale.event('del-from-cart');
+          });
+
+            jQuery('button#checkoutFormSubmit[name=confirm]').each(function() {
+                var my_funct = "getSale.event('success-order');";
+            jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+            })
+
+            if (jQuery('body.com_virtuemart.view-productdetails').length) {
+
+                (function(w, c) {
+                    w[c] = w[c] || [];
+                    w[c].push(function(getSale) {
+                        getSale.event('item-view');
+                    });
+                })(window, 'getSaleCallbacks');
+            };
+
+            if (jQuery('div.category-view').length) {
+
+                (function(w, c) {
+                    w[c] = w[c] || [];
+                    w[c].push(function(getSale) {
+                        getSale.event('cat-view');
+                    });
+                })(window, 'getSaleCallbacks');
+            };
+
+        //user reg
+            jQuery("button[name=save]").each(function() {
+                var my_funct = "getSale.event('user-reg');";
+            jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+        jQuery("button[name=register]").each(function() {
+            var my_funct = "getSale.event('user-reg');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+        })
+})
+EOD;
 
     public function __construct(& $subject, $config)
     {
         parent::__construct($subject, $config);
 
         if (!function_exists('curl_init')) {
-            echo "getSale plugin problem. Php_curl not installed.
+            echo "GetSale plugin problem. Php_curl not installed.
           Please install curl or disable plugin";
             exit();
         }
@@ -112,11 +272,52 @@ class plgSystemGetsale extends JPlugin
                   if (w.opera == "[object Opera]") {
                     d.addEventListener("DOMContentLoaded", f, false);
                   } else { f(); }
-                })(document, window, "geSaleInit");
+                })(document, window, "getSaleInit");
 
                /* GETSALE CODE END */
         ' . $this->int_scrpt;
         } else return;
+    }
+
+    public function onBeforeRender()
+    {
+    }
+
+    public function onContentPrepare($context, &$article, &$params, $page = 0)
+    {
+    }
+
+    public function onContentBeforeDisplay($context, &$product, &$params, $page = 0)
+    {
+        if ($context != 'com_virtuemart.productdetails')
+            return;
+
+        $this->productView();
+    }
+
+    private function productView()
+    {
+        if (!empty($this->projectId)) {
+            $int_scrpt = "jQuery(document).ready(function(){
+                            (function(w, c) {
+                                w[c] = w[c] || [];
+                                w[c].push(function(getSale) {
+                                    getSale.event('item-view')
+                                });
+                            })(window, 'getsaleCallbacks');
+                          })";
+            $this->int_scrpt = $int_scrpt;
+            JFactory::getDocument()->addScriptDeclaration($int_scrpt);
+        }
+    }
+
+    /** Virtuemart on add to cart
+     *
+     * @param $cart
+     */
+    public function plgVmOnAddToCart($cart)
+    {
+        $this->virtuemartSubmitCart($cart);
     }
 
     public function regbyApi()
@@ -147,5 +348,30 @@ class plgSystemGetsale extends JPlugin
         file_put_contents(JPATH_PLUGINS . '/system/getsale/log.txt', $server_output, FILE_APPEND);
         $json_result = json_decode($server_output);
         return $json_result;
+    }
+
+    public function onAfterOrderCreate(&$order, &$send_email)
+    {
+        $input = JFactory::getApplication()->input;
+        $task = $input->getCmd('task', '');
+        $ctrl = $input->getCmd('ctrl', '');
+        $option = $input->getCmd('option', '');
+
+        if ($option != 'com_hikashop' || !($ctrl == 'checkout' && $task == 'step' && !$this->isAdmin)) {
+            return;
+        }
+
+        if (!empty($this->projectId)) {
+            $int_scrpt = "jQuery(document).ready(function(){
+                            (function(w, c) {
+                                w[c] = w[c] || [];
+                                w[c].push(function(getSale) {
+                                    getSale.event('success-order')
+                                });
+                            })(window, 'getSaleCallbacks');
+                          })";
+            $this->int_scrpt = $int_scrpt;
+            JFactory::getDocument()->addScriptDeclaration($int_scrpt);
+        }
     }
 }
--- getsale.joomla/main.js
+++ getsale.joomla/main.js
@@ -15,10 +15,11 @@ function formCheck() {
 
 jQuery(document).ready(function () {
     jQuery('a[href="#description"]').hide();
-    var text_after = "Введите email и ключ API из личного кабинета getSale. <br>" +
-        "Если вы еще не регистрировались в сервисе getSale это можно сделать по ссылке <a href='http://getsale.io'>getSale.io</a>";
-    var support_text = "<br><br>Служба поддержки: <a href='mailto:plugins@getsale.io'>plugins@getsale.io</a><br>Joomla getSale v1.0.0";
-    var success_text = "Поздравляем! Ваш сайт успешно привязан к аккаунту <a href='http://getsale.io'>getSale.io</a>. Войдите в личный кабинет для просмотра статистики.";
+
+    var text_after = "Введите Email и ключ API из личного кабинета GetSale.<br>" +
+        "Если вы еще не регистрировались в сервисе GetSale это можно сделать по ссылке <a href='http://getsale.io'>GetSale</a>";
+    var support_text = "<br><br>Служба поддержки: <a href='mailto:support@getsale.io'>support@getsale.io</a><br>Joomla GetSale v1.0.0";
+    var success_text = "Поздравляем! Ваш сайт успешно привязан к аккаунту <a href='http://getsale.io'>GetSale</a>.<br/>" + "Теперь вы можете создать виджеты в личном кабинете на http://getsale.io.";
 
     if ((jQuery('#jform_params_app_key').val() !== '') && (jQuery('#jform_params_email').val() !== '')) {
         if (window.getsale_succes_reg == true && jQuery('#jform_params_getsale_id').val() !== '') {
@@ -40,16 +41,16 @@ jQuery(document).ready(function () {
             }
             if (window.getsale_reg_error == 404) {
                 jQuery('.alert.alert-success').hide();
-                var error_text = 'Данный email не зарегистрирован на сайте <a href="http://getsale.io">http://getsale.io</a>';
+                var error_text = 'Данный Email не зарегистрирован на сайте <a href="http://getsale.io">http://getsale.io</a>';
             }
             if (window.getsale_reg_error == 0) {
                 jQuery('.alert.alert-success').hide();
-                var error_text = 'Ответ от <a href="http://getsale.io">getSale</a> не был получен. Возможно, вы исползуете модуль локально!';
+                var error_text = 'Ответ от <a href="http://getsale.io">GetSale</a> не был получен. Возможно, вы исползуете модуль локально!';
             }
-            var gtsl_btn_html = '<div style="width:100%;margin-top:20px;">' +
+            var gtsl_btn_html = '<div style="width:100%;margin-top: 20px;">' +
                 '<button style="float:left;" id="gtsl_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>' +
                 '<div style="padding-top: 2px;">' +
-                '<span class="gtsl_err" style="margin-left:5px;"> <b>Ошибка!</b> ' + error_text +
+                '<span class="gtsl_err" style="margin-left: 5px;"> <b>Ошибка!</b> ' + error_text +
                 '</span>' +
                 '</div>' +
                 '</div>';
@@ -58,24 +59,33 @@ jQuery(document).ready(function () {
             if (jQuery('#jform_enabled').val() == 0) {
                 var error_text = 'Для начала работы необходимо включить плагин.';
             }
-            jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top:15px;" id="gtsl_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>' +
+            jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="gtsl_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>' +
                 '<div style="padding-top: 20px;">' +
-                '<span class="gtsl_err" style="margin-left:5px;"><b>Ошибка!</b> ' + error_text +
+                '<span class="gtsl_err" style="margin-left: 5px;"> <b>Ошибка!</b> ' + error_text +
                 '</span>' +
                 '</div>');
             jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after + support_text);
         }
     } else {
-        jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top:15px;" id="gtsl_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>');
+        jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="gtsl_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>');
         jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after + support_text);
     }
     //правим отступ
     jQuery('#jform_params_app_key').parent().css('margin-left', '70px');
-    jQuery('#jform_params_email').parent().css('margin-left', '70px');
+    jQuery('#jform_params_email').parent().css('margin-left', '70px')
 });
 
 //добавляем вместо краткого описания нужный текст.
-var text_after2 = "<p><b>getSale</b> - профессиональный инструмент для создания popup-окон.</p>" +
-    "<p>Оцените принципиально новый подход к созданию popup-окон</p>";
+var text_after2 = "<p><b>GetSale</b> - сервис повышения продаж и аналитика посетителей сайта.</p>" +
+    "<p>Оцените принципиально новый подход к просмотру статистики. Общайтесь со своей аудиторией, продавайте лучше, зарабатывайте больше. И все это бесплатно!</p>";
 jQuery('.readmore').parent().hide();
 jQuery('.info-labels').after(text_after2);
+
+if (jQuery("p.alert-message:contains('Учётная запись для вас была создана')").length) {
+    (function (w, c) {
+        w[c] = w[c] || [];
+        w[c].push(function (getSale) {
+            getSale.event('user-reg');
+        });
+    })(window, 'getSaleCallbacks');
+}
